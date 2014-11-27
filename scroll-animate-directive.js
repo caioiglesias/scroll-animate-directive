84a2f9e48f56cd95e890a6877ecdc3816412161a
Caio Iglesias <caioiglesias@gmail.com>
2014-11-27 19:58:06 -0200

    removed the need to use ng-show inside the element

--- scroll-animate-directive.js
+++ scroll-animate-directive.js
@@ -1,47 +1,54 @@
 'use strict';
 
 angular.module('scroll-animate-directive', [])
-    .directive('anmScroll', function ($window) {
-        return function (scope, element, attrs) {
-     
-            function getScrollOffsets(w) {
-     
-                // Use the specified window or the current window if no argument 
-                w = w || window;
-     
-                // This works for all browsers except IE versions 8 and before
-                if (w.pageXOffset !== null) {
+    .directive('anmScroll', function($window) {
+        return {
+            restrict: 'A',
+            transclude: true,
+            template: '<div ng-transclude ng-show=\'show\' class={{prevClass}}></div>',
+            link: function(scope, element, attrs) {
+                console.log(attrs);
+                scope.prevClass=attrs.class;
+
+                function getScrollOffsets(w) {
+
+                    // Use the specified window or the current window if no argument 
+                    w = w || window;
+
+                    // This works for all browsers except IE versions 8 and before
+                    if (w.pageXOffset !== null) {
+                        return {
+                            x: w.pageXOffset,
+                            y: w.pageYOffset
+                        };
+                    }
+
+                    // For IE (or any browser) in Standards mode
+                    var d = w.document;
+                    if (document.compatMode === 'CSS1Compat') {
+                        return {
+                            x: d.documentElement.scrollLeft,
+                            y: d.documentElement.scrollTop
+                        };
+                    }
+
+                    // For browsers in Quirks mode
                     return {
-                        x: w.pageXOffset,
-                        y: w.pageYOffset
+                        x: d.body.scrollLeft,
+                        y: d.body.scrollTop
                     };
                 }
-     
-                // For IE (or any browser) in Standards mode
-                var d = w.document;
-                if (document.compatMode === 'CSS1Compat') {
-                    return {
-                        x: d.documentElement.scrollLeft,
-                        y: d.documentElement.scrollTop
-                    };
-                }
-     
-                // For browsers in Quirks mode
-                return {
-                    x: d.body.scrollLeft,
-                    y: d.body.scrollTop
-                };
+
+                angular.element($window).bind('scroll', function() {
+                    var targetOffset = attrs.anmScroll;
+                    var offset = getScrollOffsets($window);
+                    if (offset.y >= targetOffset) {
+                        scope.show = true;
+                    } else {
+                        scope.show = false;
+                    }
+                    scope.$apply();
+                });
             }
-     
-            angular.element($window).bind('scroll', function () {
-                var targetOffset = attrs.anmScroll;
-                var offset = getScrollOffsets($window);
-                if (offset.y >= targetOffset) {
-                    scope.showme = true;
-                } else {
-                    scope.showme = false;
-                }
-                scope.$apply();
-            });
         };
     });
